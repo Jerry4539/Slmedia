70f30376aba26402a85481b72f8baae1b57628f4
Jerry_4539 <91048755+Jerry4539@users.noreply.github.com>
2021-10-03 09:00:37 +0530

    Create Slmedia.h

---
+++ Slmedia/Slmedia/Slmedia.h
@@ -0,0 +1,18 @@
+//
+//  SLMedia.h
+//  SLMedia
+//
+//  Created by => Jerry4539 on 2021/3/5.
+//  Copyright © 2021 Jerry4539 . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SLMedia.
+FOUNDATION_EXPORT double SLMediaVersionNumber;
+
+//! Project version string for SLMedia.
+FOUNDATION_EXPORT const unsigned char SLMediaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SLMedia/PublicHeader.h>
+
